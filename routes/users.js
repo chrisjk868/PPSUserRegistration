7eafd2090f973d47feb7698715fa80cc35624e05
chrisjk868 <chrisjk868@gmail.com>
2021-07-06 19:24:16 +0800

    Added Mongoose Database

--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,5 @@
+const mongoose = require('mongoose');
+const User = require("../models/user.js");
 const express = require('express');
 const router = express.Router();
 
@@ -5,16 +7,57 @@ const router = express.Router();
 router.get('/login',(req,res)=>{
     res.render('login');
 })
+
 router.get('/register',(req,res)=>{
     res.render('register')
     })
+
 //Register handle
 router.post('/register',(req,res)=>{
+  const {name,email, password, password2} = req.body;
+  let errors = [];
+  console.log(' Name ' + name+ ' email :' + email+ ' pass:' + password);
+  if(!name || !email || !password || !password2) {
+      errors.push({msg : "Please fill in all fields"})
+  }
+  //check if match
+  if(password !== password2) {
+    errors.push({msg : "passwords dont match"});
+  }
+  //check if password is more than 6 characters
+  if(password.length < 6 ) {
+    errors.push({msg : 'password atleast 6 characters'})
+  }
+  if(errors.length > 0 ) {
+    res.render('register',
+               {errors : errors,
+               name : name,
+               email : email,
+               password : password,
+               password2 : password2})
+  } else {
+    //validation passed
+    User.findOne({email : email}).exec((err, user) => {
+      console.log(user);
+      if(user) {
+        errors.push({msg: 'email has already been registered'});
+        render(res, errors, name, email, password, password2);
+      } else {
+        const newUser = new User({
+          name : name,
+          email : email,
+          password : password
+        });
+      }
+    })
+  }
 })
+
 router.post('/login',(req,res,next)=>{
   })
 
 //logout
 router.get('/logout',(req,res)=>{
  })
+
 module.exports  = router;
