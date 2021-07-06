ad7cbf4c1460fc482c20dbd68026050bf623ca96
chrisjk868 <chrisjk868@gmail.com>
2021-07-06 23:07:06 +0800

    Added bcrypt

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 const User = require("../models/users.js");
 const express = require('express');
 const router = express.Router();
